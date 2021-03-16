5b99ebb677345d910b1232cf95030f47e9a88aac
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-03-16 15:27:54 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,19 @@
+const { formDisableSelector } = require("@rails/ujs");
+
+function post(){
+  const submit = document.getElementById("submit");
+  
+
+  submit.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const form = document.getElementById("form")
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+
+}
+
+window.addEventListener('load', post);
